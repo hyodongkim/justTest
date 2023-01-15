45ba25a775cd33d887ec2efe5f0a897d4cd0b83c
hyodongkim <kimhd853@naver.com>
2023-01-15 17:53:53 +0900

    start Test

---
+++ justTest/src/main/java/com/hyodong/aqua/JustTestApplication.java
@@ -0,0 +1,13 @@
+package com.hyodong.aqua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustTestApplication.class, args);
+	}
+
+}
